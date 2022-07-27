0caa184f54e93de8110d748fe113f3a216250df7
kananb <kananboubion@gmail.com>
2022-07-26 21:55:33 -0700

    Implement basic ping/pong web server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		log.Fatal("no listening port provided")
+	}
+
+	fmt.Printf("Listening at :%v/ping ...\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%v", port), r)
 }
